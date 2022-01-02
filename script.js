438b81b305b4d61070fc1b1c3c5d65ccfdff988c
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-01-02 21:15:30 +0100

    mer animationer och design

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ let player1points = 0;
 
 let player2points = 0;
 
-gsap.from(".header", { y: -100, duration: 2.5, ease: "elastic" });
-gsap.from(".playersContainer", { x: -1000, duration: 2.5, ease: "elastic" });
-gsap.from("#pointContainer", { y: -1000, duration: 2.5, ease: "elastic" });
-gsap.from("#gameContainer", { y: 1000, duration: 2.5, ease: "elastic" });
+gsap.from(".header", { y: -100, duration: 2, ease: "elastic" });
+gsap.from(".playersContainer", { x: -1000, duration: 2, ease: "elastic" });
+gsap.from("#pointContainer", { y: -1000, duration: 2, ease: "elastic" });
+gsap.from("#gameContainer", { y: 1000, duration: 2, ease: "elastic" });
 
-gsap.from("#rockBtn", { y: -1000, duration: 2.6, delay: 0.8, ease: "bounce" });
-gsap.from("#paperBtn", { y: -1000, duration: 2.6, delay: 0.9, ease: "bounce" });
-gsap.from("#scissorBtn", { y: -1000, duration: 2.6, delay: 1, ease: "bounce" });
+gsap.from("#rockBtn", { y: -1000, duration: 2.1, delay: 0.8, ease: "bounce" });
+gsap.from("#paperBtn", { y: -1000, duration: 2.1, delay: 0.9, ease: "bounce" });
+gsap.from("#scissorBtn", { y: -1000, duration: 2.1, delay: 1, ease: "bounce" });
 
 
-gsap.from("#playerOne", { y: -1000, duration: 2.7, delay: 1.1, ease: "bounce" });
-gsap.from("#playerTwo", { y: -1000, duration: 2.7, delay: 1.2, ease: "bounce" });
+gsap.from("#playerOne", { y: -1000, duration: 2.2, delay: 1.1, ease: "bounce" });
+gsap.from("#playerTwo", { y: -1000, duration: 2.2, delay: 1.2, ease: "bounce" });
 
-gsap.from(".playerOneH1", { x: -1000, duration: 2.8, delay: 1.2, ease: "bounce" });
-gsap.from(".playerTwoH1", { x: +1000, duration: 2.8, delay: 1.2, ease: "bounce" });
+gsap.from(".playerOneH1", { x: -1000, duration: 2.3, delay: 1.2, ease: "bounce" });
+gsap.from(".playerTwoH1", { x: +1000, duration: 2.3, delay: 1.2, ease: "bounce" });
 
 
 
@@ -66,17 +66,17 @@ function playGame(yourMove) {
     let playerTwosHand = Math.floor(Math.random() * 3);
 
     if (playerOnesHand === 0 && playerTwosHand === 0) {
-        renderGame("🪨", "🪨", player1points, player2points);
+        renderGame("👊", "👊", player1points, player2points);
 
     }
 
     if (playerOnesHand === 1 && playerTwosHand === 1) {
-        renderGame("✂️", "✂️", player1points, player2points);
+        renderGame("✌️", "✌️", player1points, player2points);
 
     }
 
     if (playerOnesHand === 2 && playerTwosHand === 2) {
-        renderGame("💰", "💰", player1points, player2points);
+        renderGame("🖐", "🖐", player1points, player2points);
 
     }
 
@@ -88,12 +88,11 @@ function playGame(yourMove) {
 
         player2points++;
 
-        playerTwo.style.backgroundColor = "green";
-        playerTwo.style.fontSize = "7rem";
+        //playerTwo.style.backgroundColor = "lightgreen";
+        //playerTwo.style.fontSize = "7rem";
 
 
-
-        renderGame("✂️", "🪨", player1points, player2points);
+        renderGame("✌️", "👊", player1points, player2points);
 
 
     }
@@ -103,10 +102,10 @@ function playGame(yourMove) {
 
         player2points;
 
-        playerOne.style.backgroundColor = "green";
-        playerOne.style.fontSize = "7rem";
+        //playerOne.style.backgroundColor = "lightgreen";
+        //playerOne.style.fontSize = "7rem";
 
-        renderGame("💰", "🪨", player1points, player2points);
+        renderGame("🖐", "👊", player1points, player2points);
 
     }
 
@@ -115,10 +114,10 @@ function playGame(yourMove) {
 
         player2points;
 
-        playerOne.style.backgroundColor = "green";
-        playerOne.style.fontSize = "7rem";
+        //playerOne.style.backgroundColor = "lightgreen";
+        //playerOne.style.fontSize = "7rem";
 
-        renderGame("🪨", "✂️", player1points, player2points);
+        renderGame("👊", "✌️", player1points, player2points);
 
 
 
@@ -130,11 +129,11 @@ function playGame(yourMove) {
 
         player2points++;
 
-        playerTwo.style.backgroundColor = "green";
-        playerTwo.style.fontSize = "7rem";
+        //playerTwo.style.backgroundColor = "lightgreen";
+        //playerTwo.style.fontSize = "7rem";
 
 
-        renderGame("💰", "✂️", player1points, player2points);
+        renderGame("🖐", "✌️", player1points, player2points);
 
 
 
@@ -145,11 +144,11 @@ function playGame(yourMove) {
 
         player2points++;
 
-        playerTwo.style.backgroundColor = "green";
-        playerTwo.style.fontSize = "7rem";
+        //playerTwo.style.backgroundColor = "lightgreen";
+        //playerTwo.style.fontSize = "7rem";
 
 
-        renderGame("🪨", "💰", player1points, player2points);
+        renderGame("👊", "🖐", player1points, player2points);
 
 
 
@@ -158,11 +157,11 @@ function playGame(yourMove) {
     if (playerOnesHand === 1 && playerTwosHand === 2) {
         player1points++;
 
-        playerOne.style.backgroundColor = "green";
-        playerOne.style.fontSize = "7rem";
+        //playerOne.style.backgroundColor = "lightgreen";
+        //playerOne.style.fontSize = "7rem";
 
         player2points;
-        renderGame("✂️", "💰", player1points, player2points);
+        renderGame("✌️", "🖐", player1points, player2points);
 
     }
 
@@ -178,16 +177,25 @@ function renderGame(player1hand, player2hand, player1point, player2point) {
     playerOne.innerHTML = "";
     let player1sHand = player1hand;
     playerOne.append(player1sHand);
+    gsap.from("#playerOne", { duration: 0.4, rotation: 1440, ease: "power1" });
 
     playerTwo.innerHTML = "";
     let player2sHand = player2hand;
     playerTwo.append(player2sHand);
+    gsap.from("#playerTwo", { duration: 1.7, rotation: 8640, ease: "power1", onComplete: () => (renderPoints(player1point, player2point)) });
+
+};
+
+function renderPoints(player1point, player2point) {
 
     playerOnePoint.innerHTML = "";
     playerOnePoint.innerText = player1point;
+    playerOnePoint.style.opacity = 1
+    gsap.from("#playerOnePoint", { duration: 0.3, rotation: 360, ease: "bounce" });
 
     playerTwoPoint.innerHTML = "";
     playerTwoPoint.innerText = player2point;
-
+    playerTwoPoint.style.opacity = 1
+    gsap.from("#playerTwoPoint", { duration: 0.3, rotation: 360, ease: "bounce" });
 
 };
